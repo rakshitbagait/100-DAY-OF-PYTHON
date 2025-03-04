126fdd77306f5575a6137ea8800a46bac05fa50d
rakshitbagait <rbagait2018@gmail.com>
2025-03-04 20:59:02 +0530

    length function learned

---
+++ DAY 1/project.py
@@ -0,0 +1,6 @@
+# Band name generator
+
+print("Welcome to the Band Name generator")
+city=input("What's the name of the city you grew up in")
+petName =input("What's your pet name?")
+print("Your band name coould be",city,petName)
