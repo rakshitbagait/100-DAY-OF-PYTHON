a06b1db8e41c693f511b053a9b102278dbc995e2
rakshitbagait <rbagait2018@gmail.com>
2025-03-12 10:54:41 +0530

    Day 9 begins

---
+++ DAY 9/1.py
@@ -0,0 +1,12 @@
+# dictionaries
+programing_dictionary ={'a':"rakshit"}
+
+# adding a new element in a dictionary
+print(programing_dictionary["a"])
+programing_dictionary["b"]="Trisha"
+
+print(programing_dictionary)
+
+for key in programing_dictionary:
+    print(key)
+    print(programing_dictionary[key])
