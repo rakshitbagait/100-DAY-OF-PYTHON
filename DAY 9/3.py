7a8a7c760ec127962de75e07f238efaf84131dd4
rakshitbagait <rbagait2018@gmail.com>
2025-03-12 10:57:01 +0530

    more on dictionaries

---
+++ DAY 9/3.py
@@ -0,0 +1,26 @@
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
+
+
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
+
+dict ={
+    "a":1,
+    "b":2,
+    "c":3
+}
+
+for key in dict :
+    dict[key]+=1
+print(dict)
+
+dict [1]=4
+print(dict)
+
+print(dict[1])
