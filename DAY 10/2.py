e311a760e2605fbc2286e7a1c15ac3bb57147f0b
rakshitbagait <rbagait2018@gmail.com>
2025-03-14 10:23:08 +0530

    more on functions

---
+++ DAY 10/2.py
@@ -0,0 +1,20 @@
+# Docstring
+
+# 72. Function with outputs
+"""This is a docstring or 
+a multiline comment
+"""
+def format_name (f_name,l_name):
+    formated_f_name= f_name.title()
+    formated_l_name = l_name.title()
+    print(f"{formated_f_name} {formated_l_name}")
+
+    formated_f_name= f_name.capitalize()
+    formated_l_name = l_name.capitalize()
+
+    print(f"{formated_f_name} {formated_l_name}")
+ 
+    return (f"{formated_f_name} {formated_l_name}")
+a=format_name("rakshit","bAGait")
+
+print(a)
