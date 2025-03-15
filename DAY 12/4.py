98338bbd93817be03d7b5cad16d0f7415e816205
rakshitbagait <rbagait2018@gmail.com>
2025-03-15 12:43:21 +0530

    Global constants

---
+++ DAY 12/4.py
@@ -0,0 +1,9 @@
+#Global constants
+
+PI = 3.14159
+
+GOOGLE_URL ="https://www.udemy.com/course/100-days-of-code/learn/quiz/4871634#content"
+
+import random
+choosed_number = random.randint(1,100)
+print(choosed_number)
