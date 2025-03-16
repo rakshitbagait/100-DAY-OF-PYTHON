f4a38f93bb75454aa9b46edf9976f99c84602ed7
rakshitbagait <rbagait2018@gmail.com>
2025-03-16 09:41:31 +0530

    Try and except block

---
+++ DAY 13/2.py
@@ -0,0 +1,12 @@
+try :
+    age = int(input("How old are you?"))
+
+
+except ValueError:
+    print("you have type in an invalid number")
+    age = int(input("Enter your age"))
+
+
+if age >18:
+    print(f"you can drive at age{age}")
+
