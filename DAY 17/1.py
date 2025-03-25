9c2d98911104ec5850e8528433edcb6a33bf5a7e
rakshitbagait <rbagait2018@gmail.com>
2025-03-25 21:25:10 +0530

    Day 17

---
+++ DAY 17/1.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self,id ,username,add ):
+        self.id=id
+        self.username=username
+        self.add = add
+
+user_1= User()
+user_1.id= "001"
+user_1.username= "angela"
