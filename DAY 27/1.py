03706d19479a91957ba49a9f687e74219ad7bfbc
rakshitbagait <rbagait2018@gmail.com>
2025-05-05 16:45:46 +0530

    updated code

--- DAY 27/1.py
+++ DAY 27/1.py
@@ -8,15 +8,23 @@ my_label = tkinter.Label(text="this is my label",fg="red",bg="yellow",height=20,
 my_label.pack()
 
 my_label["text"]="new text"
+input =tkinter.Entry()
+input.pack()
 # my_label.config(text="new text")
 def button_clicked():
     print("I got clicked")
-    my_label.config(text="button is clicked")
+    new_text =input.get()
+    my_label.config(text=new_text)
+    
+    
 button = tkinter.Button(text="this is a button",height=20,width=40,highlightbackground="grey",cursor="arrow",command=button_clicked)
 button.pack(side="top")
 
 # entry
-
-input =tkinter.Entry().pack()
+def IntVar():
+    print("rakshit")
+radio_state = IntVar()
+radio = tkinter.Radiobutton(text="opetion",relief="ridge",variable=radio_state,value=1)
+radio.pack()
 
 window.mainloop() 
