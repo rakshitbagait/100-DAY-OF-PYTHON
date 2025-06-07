b81de80b2e474abd77aba9d22686faa46178c8b5
rakshitbagait <rbagait2018@gmail.com>
2025-06-07 21:16:13 +0530

    Day 39 ends

---
+++ DAY 39/flight-deals-start/flight-deals-start/notification_manager.py
@@ -0,0 +1,8 @@
+class NotificationManager:
+    # This class will send notifications with the deal flight details.
+
+    def send_sms(self, message):
+        print(f"Sending SMS: {message}")
+
+    def send_email(self, message):
+        print(f"Sending Email: {message}")
