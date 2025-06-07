490d952d0aa81097a152a9bca68e8444a7903de1
rakshitbagait <rbagait2018@gmail.com>
2025-06-07 21:13:37 +0530

    flight deal

---
+++ DAY 39/flight-deals-start/flight-deals-start/flight_data.py
@@ -0,0 +1,7 @@
+class FlightData:
+    # This class is responsible for structuring the flight data.
+
+    def iata_data(self, city_name):
+        # Placeholder: Returns "TESTING" IATA code for now
+        print(f"Fetching IATA code for {city_name}...")
+        return "TESTING"
