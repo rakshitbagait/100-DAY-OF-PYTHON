8f7bd368311ca76e6a852e3f7581b37a3f7781ec
rakshitbagait <rbagait2018@gmail.com>
2025-06-07 21:14:35 +0530

    flieght search class

---
+++ DAY 39/flight-deals-start/flight-deals-start/flight_search.py
@@ -0,0 +1,7 @@
+class FlightSearch:
+    # This class is responsible for actually searching flights (API can be added here later)
+
+    def search_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        print(f"Searching flights from {origin_city_code} to {destination_city_code} between {from_time} and {to_time}...")
+        # Placeholder
+        return "Dummy Flight Data"
